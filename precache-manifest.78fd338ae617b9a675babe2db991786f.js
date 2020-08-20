39d79ba6b170020ff9f4cd40bb7a77793d8e534b
SilentDubich <kirill.dubov.2012@mail.ru>
2020-08-20 18:54:24 +0300

    Updates

---
+++ precache-manifest.78fd338ae617b9a675babe2db991786f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a540cba8826bbffc316718e629de5003",
+    "url": "/test-funbox/index.html"
+  },
+  {
+    "revision": "3ca2e7d185249e890e3e",
+    "url": "/test-funbox/static/css/main.1627f08f.chunk.css"
+  },
+  {
+    "revision": "50f513e98817534fcbdf",
+    "url": "/test-funbox/static/js/2.83e3f815.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-funbox/static/js/2.83e3f815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca2e7d185249e890e3e",
+    "url": "/test-funbox/static/js/main.681a9503.chunk.js"
+  },
+  {
+    "revision": "87c0281bc05bece25f20",
+    "url": "/test-funbox/static/js/runtime-main.200e4444.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "1717a88a2f0e0f7988c3963cfaf40ed2",
+    "url": "/test-funbox/static/media/background.1717a88a.png"
+  },
+  {
+    "revision": "a3250c592d59a1ae07458bbcf91c1191",
+    "url": "/test-funbox/static/media/cat.a3250c59.png"
+  }
+]);
